8c6e5fe3180e686367bfb70308beea9c163dcf48
Tor Gikling <torog@stud.ntnu.no>
2022-02-11 10:48:38 +0100

    init commit

---
+++ src/main/java/no/ntnu/gr12/krrr_project/KrrrProjectApplication.java
@@ -0,0 +1,13 @@
+package no.ntnu.gr12.krrr_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KrrrProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KrrrProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/no/ntnu/gr12/krrr_project/KrrrProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package no.ntnu.gr12.krrr_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KrrrProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
