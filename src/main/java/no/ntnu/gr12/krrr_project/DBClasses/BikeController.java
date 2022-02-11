e1c15b379d3c56ddb412246203a9eeef512c6b1b
Streafe <streafe@gmail.com>
2022-02-11 12:18:11 +0100

    Fixed BikeController

--- src/main/java/no/ntnu/gr12/krrr_project/DBClasses/BikeController.java
+++ src/main/java/no/ntnu/gr12/krrr_project/DBClasses/BikeController.java
@@ -2,6 +2,9 @@ package no.ntnu.gr12.krrr_project.DBClasses;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.List;
 
@@ -10,7 +13,28 @@ public class BikeController {
     @Autowired
     private BikeService bikeService;
 
-    public List<Bike> getBike(@PathVariable String id){
+    @RequestMapping("/bikes")
+    public List<Bike> getBikes(){
+        return bikeService.getBikes();
+    }
 
+    @RequestMapping("/bikes/{id}")
+    public Bike getBike(@PathVariable String id){
+        return bikeService.getBike(id);
+    }
+
+    @RequestMapping(method = RequestMethod.POST,value = "/bikes")
+    public void addBike(@RequestBody Bike bike){
+        bikeService.addBike(bike);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT,value = "/bikes/{id}")
+    public void updateBike(@RequestBody Bike bike,@PathVariable String id){
+        bikeService.updateBike(id,bike);
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE,value = "/bikes/{id}")
+    public void deleteBike(@PathVariable String id){
+        bikeService.deleteBike(id);
     }
 }
