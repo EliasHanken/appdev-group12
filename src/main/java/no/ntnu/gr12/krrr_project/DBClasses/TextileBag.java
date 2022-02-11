ac82334204cab311cee2f8064357aba53b5f952b
Tor Gikling <torog@stud.ntnu.no>
2022-02-11 14:08:32 +0100

    Added sunglasses and Textilebag class

---
+++ src/main/java/no/ntnu/gr12/krrr_project/DBClasses/TextileBag.java
@@ -0,0 +1,16 @@
+package no.ntnu.gr12.krrr_project.DBClasses;
+
+/**
+ * Class containing the textileBag
+ */
+public class TextileBag extends Item{
+
+    public TextileBag() {
+        super();
+    }
+
+    public TextileBag(String textileBagID, String modelNumber, float price) {
+        super(textileBagID, modelNumber, price);
+    }
+
+}
