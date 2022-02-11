ea132781e9bb322de89ff31078f9c6b7db020323
Thomas <thomasey@stud.ntnu.no>
2022-02-11 13:37:42 +0100

    Added helmetService and helmetController classes
    HelmetController not yet implemented.
    Somewhat experimental.

---
+++ src/main/java/no/ntnu/gr12/krrr_project/DBClasses/HelmetController.java
@@ -0,0 +1,9 @@
+package no.ntnu.gr12.krrr_project.DBClasses;
+
+/**
+ * Controller class for Helmet objects, used to handle CRUD functionality for helmet objects.
+ * @author Thomas Ystenes
+ */
+public class HelmetController {
+    
+}
---
+++ src/main/java/no/ntnu/gr12/krrr_project/DBClasses/HelmetService.java
@@ -0,0 +1,65 @@
+package no.ntnu.gr12.krrr_project.DBClasses;
+
+import java.util.AbstractList;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class HelmetService {
+
+    /**
+     * Creates two helmet objects for testing purposes.
+     */
+    private List<Helmet> helmets = new ArrayList<>(Arrays.asList(
+            new Helmet("002001", "201", 200),
+            new Helmet("002002", "202", 250)
+    ));
+
+    /**
+     * @return all objects in helmets list.
+     */
+    public List<Helmet> getHelmets(){
+        return helmets;
+    }
+
+    /**
+     * Returns helmet with specific ID
+     * @param id the ID of the helmet that should be returned.
+     * @return The helmet with the corresponding ID.
+     */
+    public Helmet getHelmet(String id){
+        return helmets.stream().filter(e -> e.getItemID().equals(id)).findFirst().orElse(null);
+    }
+
+    /**
+     * Adds helmet to list of helmets.
+     * @param helmet object to be added to helmets.
+     */
+    public void addHelmet(Helmet helmet){
+        helmets.add(helmet);
+    }
+
+    /**
+     * Updates helmets list.
+     * @param id
+     * @param helmet
+     */
+    public void updateHelmet(String id, Helmet helmet){
+        for(int i = 0; i < helmets.size(); i++){
+            Helmet h = helmets.get(i);
+            if(h.getItemID().equals(id)){
+                helmet.setItemID(h.getItemID());
+                return;
+            }
+        }
+    }
+
+    /**
+     * Deletes helmet with corresponding id from helmets.
+     * @param id The ID of the helmet to be deleted.
+     */
+    public void deleteHelmet(String id){
+        helmets.removeIf(h -> h.getItemID().equals(id));
+    }
+
+}
