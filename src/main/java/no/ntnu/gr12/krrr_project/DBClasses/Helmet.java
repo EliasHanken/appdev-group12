e64ddd2154f2cfbbe999847b62ebbbfceab4d034
Tor Gikling <torog@stud.ntnu.no>
2022-02-11 13:43:33 +0100

    Readded the Helmet class

---
+++ src/main/java/no/ntnu/gr12/krrr_project/DBClasses/Helmet.java
@@ -0,0 +1,16 @@
+package no.ntnu.gr12.krrr_project.DBClasses;
+/**
+ * Class for the Helmet that contains it`s price modelNumber and ID
+ *
+ * @author Tor Oveland Gikling
+ * @version 0.0.1
+ */
+public class Helmet extends Item {
+    public Helmet() {
+        super();
+    }
+
+    public Helmet(String helmetID, String modelNumber, float price) {
+        super(helmetID, modelNumber, price);
+    }
+}
