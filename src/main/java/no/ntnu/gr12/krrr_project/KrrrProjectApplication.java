9cfe9ce4e5ec180b5e07931c9cc2dd7046cd485e
Streafe <streafe@gmail.com>
2022-03-08 14:07:49 +0100

    Added swagger for api documentation

--- src/main/java/no/ntnu/gr12/krrr_project/KrrrProjectApplication.java
+++ src/main/java/no/ntnu/gr12/krrr_project/KrrrProjectApplication.java
@@ -6,6 +6,13 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Collections;
 
 @SpringBootApplication
 public class KrrrProjectApplication {
@@ -27,5 +34,36 @@ public class KrrrProjectApplication {
             insertDummyData(repository);
             System.out.println(repository.findAll());
         });
+    }
+
+    /**
+     * Create configuration for Swagger - turn off some Spring-default APIs etc
+     * @return Swagger config
+     */
+    @Bean
+    public Docket getSwaggerConfiguration() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                // We can say that we want to include only APIs under a specific URL. But we skip that, because
+                // here all the APIs which are created by us use the no.ntnu package
+                // See some examples of URL matching rules here:
+                // https://stackoverflow.com/questions/2952196/ant-path-style-patterns
+//				.paths(PathSelectors.ant("/products/**"))
+                .apis(RequestHandlerSelectors.basePackage("no.ntnu"))
+                .build()
+                .apiInfo(describeApi());
+    }
+
+    private ApiInfo describeApi() {
+        return new ApiInfo(
+                "Appedev group12 backend for Bike Rental",
+                "Spring boot backend for bike rental project (group12). Used in course IDATA2306 Application development",
+                "1.0",
+                "https://github.com/Streafe/appdev-group12.git",
+                new Contact("Group12", null, null),
+                "MIT License",
+                null,
+                Collections.emptyList()
+        );
     }
 }
