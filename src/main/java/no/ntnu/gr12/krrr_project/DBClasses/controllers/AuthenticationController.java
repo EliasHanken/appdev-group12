f05ac70cf49b092dc2210502f43f30ed53ae5278
Streafe <streafe@gmail.com>
2022-03-31 12:37:39 +0200

    user login tokens and roles

--- src/main/java/no/ntnu/gr12/krrr_project/DBClasses/controllers/AuthenticationController.java
+++ src/main/java/no/ntnu/gr12/krrr_project/DBClasses/controllers/AuthenticationController.java
@@ -1,7 +1,17 @@
 package no.ntnu.gr12.krrr_project.DBClasses.controllers;
 
+import no.ntnu.gr12.krrr_project.DBClasses.security.AuthenticationRequest;
+import no.ntnu.gr12.krrr_project.DBClasses.security.AuthenticationResponse;
 import no.ntnu.gr12.krrr_project.DBClasses.security.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,7 +19,21 @@ public class AuthenticationController {
     @Autowired
     private AuthenticationManager authenticationManager;
     @Autowired
-    UserDatailsService userDatailsService;
+    UserDetailsService userDetailsService;
     @Autowired
     private JwtUtil jwtUtil;
+
+    public ResponseEntity<?> authenticate(@RequestBody AuthenticationRequest authenticationRequest){
+        try{
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+                    authenticationRequest.getUsername(),
+                    authenticationRequest.getPassword()
+            ));
+        }catch (BadCredentialsException e){
+            return new ResponseEntity<>("Invalid username or password", HttpStatus.UNAUTHORIZED);
+        }
+        final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getUsername());
+        final String jwt = jwtUtil.generateToken(userDetails);
+        return ResponseEntity.ok(new AuthenticationResponse(jwt));
+    }
 }
--- src/main/java/no/ntnu/gr12/krrr_project/DBClasses/security/JwtUtil.java
+++ src/main/java/no/ntnu/gr12/krrr_project/DBClasses/security/JwtUtil.java
@@ -1,7 +1,14 @@
 package no.ntnu.gr12.krrr_project.DBClasses.security;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.function.Function;
 
 @Component
 public class JwtUtil {
@@ -12,11 +19,54 @@ public class JwtUtil {
      */
     private static final String JWT_AUTH_KEY = "roles";
 
+
+    /**
+     * @param userDetails Object containing user details.
+     * @return JWT token string
+     */
     public String generateToken(UserDetails userDetails){
         final long TIME_NOW = System.currentTimeMillis();
         final long MILLISECONDS_IN_HOUR = 60*60*1000;
         final long TIME_AFTER_ONE_HOUR = TIME_NOW + MILLISECONDS_IN_HOUR;
 
-        return Jwts.builder();
+        return Jwts.builder()
+                .setSubject(userDetails.getUsername())
+                .claim(JWT_AUTH_KEY, userDetails.getAuthorities())
+                .setIssuedAt(new Date(TIME_NOW))
+                .setExpiration(new Date(TIME_AFTER_ONE_HOUR))
+                .signWith(SignatureAlgorithm.HS256,SECRET_KEY)
+                .compact();
+    }
+
+    /**
+     * Find username from JWT token
+     *
+     * @param token JWT token
+     * @return Username
+     */
+    public String extractUsername(String token){
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public Boolean validateToken(String token,UserDetails userDetails){
+        final String username = extractUsername(token);
+        return username.equals(userDetails.getUsername()) && !isTokenExpired(token);
+    }
+
+    private Date extractExpiration(String token){
+        return extractClaim(token,Claims::getExpiration);
+    }
+
+    private <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).getBody();
+    }
+
+    private Boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
     }
 }
