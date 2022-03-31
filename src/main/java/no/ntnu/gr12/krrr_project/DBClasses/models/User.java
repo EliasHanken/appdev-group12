5cc11b43dbd37125fc1c5b8a7d5aef659e890043
Thomas <thomasystenes@gmail.com>
2022-03-31 12:38:18 +0200

    Update user class

--- src/main/java/no/ntnu/gr12/krrr_project/DBClasses/models/User.java
+++ src/main/java/no/ntnu/gr12/krrr_project/DBClasses/models/User.java
@@ -1,46 +1,93 @@
 package no.ntnu.gr12.krrr_project.DBClasses.models;
 
-import jdk.jshell.execution.Util;
-import org.springframework.format.annotation.DateTimeFormat;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import java.util.Date;
 
 @Entity
-@Table(name="users")
+@Table(name = "users")
 public class User {
-    @Id
-    private String userID;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String username;
+  private String password;
+  private boolean active = true;
 
-    private String description;
+  @ManyToMany(fetch = FetchType.EAGER)
+  @JoinTable(name = "user_role",
+    joinColumns = @JoinColumn(name="user_id"),
+    inverseJoinColumns = @JoinColumn(name="role_id")
+  )
 
-    public User() {
+  private Set<Role> roles = new LinkedHashSet<>();
 
-    }
+  /**
+   * Empty constructor needed for JPA
+   */
+  public User() {
+  }
 
-    public String getUserID() {
-        return userID;
-    }
+  public User(String username, String password) {
+    this.username = username;
+    this.password = password;
+  }
 
-    public void setUserID(String userID) {
-        this.userID = userID;
-    }
+  public Long getId() {
+    return id;
+  }
 
-    public String getDescription() {
-        return description;
-    }
+  public void setId(Long id) {
+    this.id = id;
+  }
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
+  public String getUsername() {
+    return username;
+  }
 
-    @Override
-    public String toString() {
-        return "user{" + "" +
-                "id=" + userID +
-                ", description='" + description + "\'" +
-                "}";
-    }
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public boolean isActive() {
+    return active;
+  }
+
+  public void setActive(boolean active) {
+    this.active = active;
+  }
+
+  public Set<Role> getRoles() {
+    return roles;
+  }
+
+  public void setRoles(Set<Role> roles) {
+    this.roles = roles;
+  }
+
+  /**
+   * Adds role to user
+   * @param role Role which should be added to user.
+   */
+  public void addRole(Role role) {
+    roles.add(role);
+  }
 }
