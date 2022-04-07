4517df05ddceb95dfa584d76f4d7a1640a17da9f
Streafe <streafe@gmail.com>
2022-04-07 14:09:15 +0200

    Fixed register endpoint

--- src/main/java/no/ntnu/gr12/krrr_project/DBClasses/controllers/RegisterController.java
+++ src/main/java/no/ntnu/gr12/krrr_project/DBClasses/controllers/RegisterController.java
@@ -1,20 +1,26 @@
 package no.ntnu.gr12.krrr_project.DBClasses.controllers;
 
 import no.ntnu.gr12.krrr_project.DBClasses.models.User;
+import no.ntnu.gr12.krrr_project.DBClasses.repositories.UserRepository;
 import no.ntnu.gr12.krrr_project.DBClasses.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RegisterController {
 
     @Autowired
-    private UserService userService;
+    private UserRepository userRepository;
 
     @RequestMapping("/register")
-    public void register(@RequestBody User user){
-        userService.addUser(user);
+    public void register(@RequestParam String name, @RequestParam String password){
+        User test = new User();
+        test.setId(543L);
+        test.setDescription("idk");
+        test.setUsername(name);
+        test.setPassword(password);
     }
 }
